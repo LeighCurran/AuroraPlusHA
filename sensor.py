5a9a864689f67cb2451fa00eb33f77456db03026
LeighCurran <leigh.curran@outlook.com>
2021-07-09 21:17:52 +1000

    Alpha

--- sensor.py
+++ sensor.py
@@ -7,16 +7,15 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import (
-    #ATTR_NAME,
+    ATTR_NAME,
     CONF_USERNAME,
     CONF_PASSWORD,
-    CONF_NAME
+    CONF_NAME,
+    CURRENCY_DOLLAR
 )
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
-
-SENSOR_NAME = "Aurora+"
 
 SCAN_INTERVAL = timedelta(hours=6)
 
@@ -29,59 +28,57 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Aurora+ platform for sensors."""
+
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
 
     try:
         api = auroraplus.api(username, password)
-        add_entities([AuroraAccountSensor(api)], True)
+        add_entities([AuroraAccountSensor(username, password)], True)
     except OSError as err:
         _LOGGER.error("Connection to Aurora+ failed: %s", err)
         return False
-
+    
 
 class AuroraAccountSensor(SensorEntity):
     """Representation of a Aurora+ sensor."""
 
-    def __init__(self, api):
+    def __init__(self, username, password):
         """Initialize the Aurora+ sensor."""
-        self._unique_id = api.serviceAgreementID
-        self._auroraplus = api
-        self._state = None
+        self.username = username
+        self.password = password
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return SENSOR_NAME
+        return 'Auroraplus UsageDaysRemaining'
 
-    @property
-    def unique_id(self):
+    #@property
+    #def unique_id(self):
         """Return unique ID for the sensor."""
-        return self._unique_id
+    #    return 'test'
 
     @property
     def state(self):
         """Return the state of the sensor."""
         return self._state
-    
-    #@property
-    # extra_state_attributes(self):
-        """Return the state attributes."""
-    #    attrs = {
-    #        ATTR_NAME: self._name
-    #    }
-
-    #    return attrs
 
     @property
     def icon(self):
         """Return the icon to use in the frontend."""
-        return "mdi:power-plug-outline"
+        return "mdi:power-socket-au"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return CURRENCY_DOLLAR
 
     def update(self):
         """Collect updated data from Aurora+ API."""
         try:
-            self._state = self.api.AverageDailyUsage
+            AuroraPlus = auroraplus.api(self.username , self.password)
+            AuroraPlus.getcurrent()
+            self._state = AuroraPlus.UsageDaysRemaining
         except OSError as err:
             _LOGGER.error("Connection to Aurora+ failed: %s", err)
         return False
