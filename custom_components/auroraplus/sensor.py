4a6a71f5c1597047bda158f27793fe5b92628f78
Olivier Mehani <shtrom@ssji.net>
2023-08-03 19:44:50 +1000

    Split out AuroraPlus API coordinator
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- custom_components/auroraplus/sensor.py
+++ custom_components/auroraplus/sensor.py
@@ -94,21 +94,52 @@ async def async_setup_platform(hass, config,
     except OSError as err:
         _LOGGER.error("Connection to Aurora+ failed: %s", err)
 
+    aurora_api = AuroraApi(hass, AuroraPlus)
+
     async_add_entities([
         AuroraSensor(hass,
                      sensor, name,
-                     AuroraPlus, rounding)
+                     aurora_api, rounding)
         for sensor in config.get(CONF_MONITORED_CONDITIONS)
     ],
         True)
 
 
+class AuroraApi():
+    """Asynchronously-updating wrapper for the Aurora API. """
+    _hass = None
+    _session = None
+
+    def __init__(self, hass, auroraplus):
+        self._hass = hass
+        self._session = auroraplus
+
+    # @Throttle(DEFAULT_SCAN_INTERVAL)  # XXX: should be configurable
+    async def async_update(self):
+        await self._hass.async_add_executor_job(self._api_update)
+
+    def _api_update(self):
+        try:
+            self._session.getcurrent()
+            self._session.getsummary()
+            self._session.getday()
+        except Exception as e:
+            _LOGGER.warn(f'Error updating data: {e}')
+        _LOGGER.debug('Updated data')
+
+    def __getattr__(self, attr):
+        """Forward any attribute access to the session, or handle error """
+        try:
+            return getattr(self._session, attr)
+        except AttributeError:
+            _LOGGER.debug(f'Data for {attr} not yet available')
+            return {}  # an empty thing that has `get`
 
 
 class AuroraSensor(SensorEntity):
     """Representation of a Aurora+ sensor."""
 
-    def __init__(self, hass, sensor, name, auroraplus, rounding):
+    def __init__(self, hass, sensor, name, aurora_api, rounding):
         """Initialize the Aurora+ sensor."""
         self._hass = hass
         self._name = name + ' ' + sensor
@@ -116,7 +147,7 @@ class AuroraSensor(SensorEntity):
         self._unit_of_measurement = None
         self._state = None
         self._last_reset = None
-        self._session = auroraplus
+        self._api = aurora_api
         self._uniqueid = self._name
         self._rounding = rounding
         _LOGGER.debug("Created sensor %s", self._sensor)
@@ -165,47 +196,38 @@ class AuroraSensor(SensorEntity):
     def extra_state_attributes(self):
         """Return device state attributes."""
         if self._sensor == SENSOR_DOLLARVALUEUSAGE:
-            return self._session.DollarValueUsage
+            return self._api.DollarValueUsage
         elif self._sensor == SENSOR_KILOWATTHOURUSAGE:
-            return self._session.KilowattHourUsage
+            return self._api.KilowattHourUsage
         elif self._sensor == SENSOR_ESTIMATEDBALANCE:
             attributes = {}
-            attributes['Amount Owed'] = self._session.AmountOwed
-            attributes['Average Daily Usage'] = self._session.AverageDailyUsage
-            attributes['Usage Days Remaining'] = self._session.UsageDaysRemaining
-            attributes['Actual Balance'] = self._session.ActualBalance
-            attributes['Unbilled Amount'] = self._session.UnbilledAmount
-            attributes['Bill Total Amount'] = self._session.BillTotalAmount
-            attributes['Number Of Unpaid Bills'] = self._session.NumberOfUnpaidBills
-            attributes['Bill Overdue Amount'] = self._session.BillOverDueAmount
+            attributes['Amount Owed'] = self._api.AmountOwed
+            attributes['Average Daily Usage'] = self._api.AverageDailyUsage
+            attributes['Usage Days Remaining'] = self._api.UsageDaysRemaining
+            attributes['Actual Balance'] = self._api.ActualBalance
+            attributes['Unbilled Amount'] = self._api.UnbilledAmount
+            attributes['Bill Total Amount'] = self._api.BillTotalAmount
+            attributes['Number Of Unpaid Bills'] = self._api.NumberOfUnpaidBills
+            attributes['Bill Overdue Amount'] = self._api.BillOverDueAmount
             return attributes
 
     async def async_update(self):
         """Collect updated data from Aurora+ API."""
-        def _api_update():
-          try:
-              _LOGGER.debug("Updating sensor: %s", self._sensor)
-              self._session.getcurrent()
-              self._session.getsummary()
-              self._data = self._session
-          except OSError as err:
-              _LOGGER.error("Updating Aurora+ failed: %s", err)
-
-        await self._hass.async_add_executor_job(_api_update)
+        await self._api.async_update()
 
         self._old_state = self._state
         if self._sensor == SENSOR_ESTIMATEDBALANCE:
             self._state = round(
-                float(self._session.EstimatedBalance), self._rounding)
+                float(self._api.EstimatedBalance), self._rounding)
         elif self._sensor == SENSOR_DOLLARVALUEUSAGE:
             self._state = round(
-                self._session.DollarValueUsage['Total'], self._rounding)
+                self._api.DollarValueUsage['Total'], self._rounding)
         elif self._sensor == SENSOR_KILOWATTHOURUSAGE:
             self._state = round(
-                self._session.KilowattHourUsage['Total'], self._rounding)
+                self._api.KilowattHourUsage['Total'], self._rounding)
         elif self._sensor.startswith(SENSOR_KILOWATTHOURUSAGETARIFF):
             tariff = self._sensor.removeprefix(SENSOR_KILOWATTHOURUSAGETARIFF)
-            self._state = self._session.KilowattHourUsage.get(tariff)
+            self._state = self._api.KilowattHourUsage.get(tariff)
             if self._state:
                 self._state = round(self._state, self._rounding)
         else:
