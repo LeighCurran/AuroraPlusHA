cca06d9866747b2324ae106aea4fbc7adf2da40b
Olivier Mehani <shtrom@ssji.net>
2025-06-21 21:50:21 +1000

    Use hass.config_entries.async_forward_entry_setups
    This is required from HA 2025.06 [0].
    
    [0] https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/

--- custom_components/auroraplus/__init__.py
+++ custom_components/auroraplus/__init__.py
@@ -43,8 +43,8 @@ async def async_setup_entry(hass, entry):
 
     entry.runtime_data = AuroraApi(hass, api_session)
 
-    await hass.config_entries.async_forward_entry_setup(
-        entry, "sensor"
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["sensor"]
     )
 
     return True
