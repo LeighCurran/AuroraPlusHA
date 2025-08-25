c060eb43403eb1a98a12ee99e77714a29c2690a1
Olivier Mehani <shtrom@ssji.net>
2025-08-25 21:16:15 +1000

    lint: ruff check --fix + manual fixes

--- custom_components/auroraplus/__init__.py
+++ custom_components/auroraplus/__init__.py
@@ -1,22 +1,10 @@
 """The auroraplus sensor integration."""
+
 import logging
 
-from homeassistant.components.sensor.const import (
-    SensorDeviceClass,
-)
-from homeassistant.const import (
-    CONF_USERNAME,
-    CONF_PASSWORD,
-    CONF_ACCESS_TOKEN,
-    CONF_NAME,
-    CONF_MONITORED_CONDITIONS,
-    CURRENCY_DOLLAR,
-    CONF_SCAN_INTERVAL,
-    UnitOfEnergy,
-)
 from homeassistant.exceptions import (
-        ConfigEntryNotReady,
-        PlatformNotReady,
+    ConfigEntryNotReady,
+    PlatformNotReady,
 )
 
 
@@ -25,6 +13,7 @@ from .const import CONF_TOKEN, CONF_ID_TOKEN, DOMAIN
 from .coordinator import AuroraPlusCoordinator
 
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass, entry):
     """Set up entry."""
@@ -35,13 +24,9 @@ async def async_setup_entry(hass, entry):
     token = entry.data.get(CONF_TOKEN)
 
     try:
-        api = await hass.async_add_executor_job(
-            aurora_init,
-            token,
-            id_token
-        )
+        api = await hass.async_add_executor_job(aurora_init, token, id_token)
     except OSError as err:
-        raise PlatformNotReady('Connection to Aurora+ failed') from err
+        raise PlatformNotReady("Connection to Aurora+ failed") from err
 
     entry.async_on_unload(
         entry.add_update_listener(AuroraPlusCoordinator.update_listener)
@@ -50,13 +35,11 @@ async def async_setup_entry(hass, entry):
     entry.runtime_data = AuroraPlusCoordinator(hass, entry, api)
 
     if not (
-            hasattr(entry.runtime_data, "week")
-            and entry.runtime_data.week.get("TariffTypes")
-            ):
+        hasattr(entry.runtime_data, "week")
+        and entry.runtime_data.week.get("TariffTypes")
+    ):
         raise ConfigEntryNotReady("No tariffs in returned data, yet")
 
-    await hass.config_entries.async_forward_entry_setups(
-        entry, ["sensor"]
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
